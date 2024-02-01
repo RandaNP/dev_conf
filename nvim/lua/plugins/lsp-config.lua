36a3981d87e39172ac4788c0fd50e9a68ffbbcd7
RandaNP <marco.randon@gmail.com>
2024-02-02 00:01:51 +0100

    Fixed completions for installed language servers

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -2,34 +2,37 @@ return {
 	{
 		-- Automatically install LSPs to stdpath for neovim
 		"williamboman/mason.nvim",
+    lazy = false,
 		config = function()
-			require("mason").setup({})
+			require("mason").setup()
 		end,
 	},
 	{
 		"williamboman/mason-lspconfig.nvim",
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "tsserver" },
-			})
-		end,
+    lazy = false,
+    opts = {
+      auto_install = true,
+    },
 	},
 	{
 		-- Useful status updates for LSP
 		"j-hui/fidget.nvim",
+    lazy = false,
 		config = function()
-			require("fidget").setup({})
+			require("fidget").setup()
 		end,
 	},
 	{
 		-- Additional lua configuration, makes nvim stuff amazing
 		"folke/neodev.nvim",
+    lazy = false,
 		config = function()
-			require("neodev").setup({})
+			require("neodev").setup()
 		end,
 	},
 	{
 		"neovim/nvim-lspconfig",
+    lazy = false,
 		config = function()
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
