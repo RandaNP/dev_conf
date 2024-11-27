74fc6a6eb63d90ebf9efc05d48905cb19532107a
Marco Randon <marco.randon@maregroup.it>
2024-11-27 16:21:31 +0100

    Wezterm - added WSL2 clipboard configuration

--- wezterm.lua
+++ wezterm.lua
@@ -1,5 +1,5 @@
 -- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 local act = wezterm.action
 
 -- This table will hold the configuration.
@@ -8,26 +8,40 @@ local config = {}
 -- In newer versions of wezterm, use the config_builder which will
 -- help provide clearer error messages
 if wezterm.config_builder then
-  config = wezterm.config_builder()
+	config = wezterm.config_builder()
 end
 
 -- This is where you actually apply your config choices
 
-config.color_scheme = 'Catppuccin Mocha'
+config.color_scheme = "Catppuccin Mocha"
 config.enable_tab_bar = false
 config.window_padding = {
-  left = '2px',
-  right = '2px',
-  top = '1px',
-  bottom = '1px',
+	left = "2px",
+	right = "2px",
+	top = "1px",
+	bottom = "1px",
 }
-config.font =
-    wezterm.font("SauceCodePro Nerd Font Mono", {
-      weight = "Regular",
-      stretch = "Normal",
-      style = "Normal"
-    })
+config.font = wezterm.font("SauceCodePro Nerd Font Mono", {
+	weight = "Regular",
+	stretch = "Normal",
+	style = "Normal",
+})
 config.font_size = 14
+if os.getenv("WSL_DISTRO_NAME") ~= nil then
+	config.keys = {
+		-- paste freom the clipboard
+		-- { key = "V", mods = "CTRL|SHIFT", action = act.PasteFrom("Clipboard") },
+		{ key = "V", mods = "CTRL|SHIFT", action = act.EmitEvent("wsl-paste-workaround") },
+	}
+	wezterm.on("wsl-paste-workaround", function(window, pane)
+		local success, stdout, stderr = wezterm.run_child_process({ "wl-paste", "--no-newline" })
+		if success then
+			pane:paste(stdout)
+		else
+			wezterm.log_error("wl-paste failed with\n" .. stderr .. stdout)
+		end
+	end)
+end
 
 -- and finally, return the configuration to wezterm
 return config
