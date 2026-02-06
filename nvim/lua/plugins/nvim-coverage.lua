91f1c00bb5e1348c7f4489dd44466c164619974f
Marco Randon <marco.randon@maregroup.it>
2026-02-06 17:34:57 +0100

    feat: add nvim-coverage plugin
    This commit adds the nvim-coverage plugin to enhance code coverage
    analysis within Neovim.
    
    The plugin provides the following features:
    - Automatic reloading of coverage data.
    - Keybindings for:
      - Loading and displaying coverage.
      - Hiding coverage.
      - Displaying a coverage summary.
      - Jumping to the next/previous uncovered or partially covered line.

---
+++ nvim/lua/plugins/nvim-coverage.lua
@@ -0,0 +1,67 @@
+return {
+  {
+    "andythigpen/nvim-coverage",
+    version = "*",
+    config = function()
+      require("coverage").setup({
+        auto_reload = true,
+      })
+    end,
+    keys = {
+      {
+        "<leader>tc",
+        "",
+        desc = "Test coverage",
+      },
+      {
+        "<leader>tcl",
+        function()
+          require("coverage").load(true)
+        end,
+        desc = "Coverage show",
+      },
+      {
+        "<leader>tch",
+        function()
+          require("coverage").clear()
+        end,
+        desc = "Coverage hide",
+      },
+      {
+        "<leader>tcs",
+        function()
+          require("coverage").summary()
+        end,
+        desc = "Coverage summary",
+      },
+      {
+        "<leader>tcn",
+        function()
+          require("coverage").jump_next("uncovered")
+        end,
+        desc = "Next uncovered",
+      },
+      {
+        "<leader>tcN",
+        function()
+          require("coverage").jump_next("partial")
+        end,
+        desc = "Next partial covered",
+      },
+      {
+        "<leader>tcp",
+        function()
+          require("coverage").jump_prev("uncovered")
+        end,
+        desc = "Prev uncovered",
+      },
+      {
+        "<leader>tcP",
+        function()
+          require("coverage").jump_next("partial")
+        end,
+        desc = "Prev partial covered",
+      },
+    },
+  },
+}
