2facb43dbeda61d55caf1b119c293f3d208e1830
Marco Randon <marco.randon@maregroup.it>
2026-02-06 17:33:39 +0100

    feat: add dap configuration for python
    This commit adds a new DAP configuration for Python to the nvim-dap
    plugin.
    The configuration allows users to debug Python code with the following
    features:
    
    - Launch debug.py
    - Set the Python path to either the virtual environment or the system
    Python
    - Use the integrated terminal
    - Set the current working directory to the workspace folder
    - Enable justMyCode
    
    It also defines some basic keymaps for DAP.

---
+++ nvim/lua/plugins/dap.lua
@@ -0,0 +1,60 @@
+return {
+  {
+    "mfussenegger/nvim-dap",
+    opts = function()
+      local dap = require("dap")
+
+      -- Define the Python configuration
+      dap.configurations.python = dap.configurations.python or {}
+      table.insert(dap.configurations.python, {
+        type = "python",
+        request = "launch",
+        name = "Taipy: Launch debug.py",
+        program = "${workspaceFolder}/debug.py",
+        pythonPath = function()
+          local cwd = vim.fn.getcwd()
+          if vim.fn.executable(cwd .. "/.venv/bin/python") == 1 then
+            return cwd .. "/.venv/bin/python"
+          else
+            return "python"
+          end
+        end,
+        console = "integratedTerminal",
+        cwd = "${workspaceFolder}",
+        justMyCode = true,
+      })
+    end,
+    keys = {
+      -- LazyVim already defines many DAP keys, but we can add/override them here
+      {
+        "<F5>",
+        function()
+          require("dap").continue()
+        end,
+        desc = "Continue",
+      },
+      {
+        "<F10>",
+        function()
+          require("dap").step_over()
+        end,
+        desc = "Step Over",
+      },
+      {
+        "<F11>",
+        function()
+          require("dap").step_into()
+        end,
+        desc = "Step Into",
+      },
+      {
+        "<F12>",
+        function()
+          require("dap").step_out()
+        end,
+        desc = "Step Out",
+      },
+      --   { "<leader>dq", function() require("dap").terminate() end, desc = "Terminate Session" },
+    },
+  },
+}
